c1c5715cf461b96b0cf18c3893ef7011665823ac
Galushko Evgeniy <yudginchik@gmail.com>
2024-07-12 16:00:49 +0300

    add task-3 file

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,19 @@
 'use strict';
 
+
+function slugify(title) {
+  const smallFont = title.toLowerCase();
+  const conversionToArray = smallFont.split(" ")
+  const result = conversionToArray.join("-");
+  return result;
+}
+
+
+console . log (slugify( "Arrays for begginers" )); // "arrays-for-begginers" 
+console . log (slugify( "English for developer" )); // "english-for-developer" 
+console . log (slugify( "Ten secrets of JavaScript" )); // "ten-secrets-of-javascript" 
+console . log (slugify( "How to become a JUNIOR developer in TWO WEEKS" )); // "how-to-become-a-junior-developer-in-two-weeks"
+
+
+
+
--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,18 @@
 'use strict';
 
+function makeArray(firstArray, secondArray, maxLength) {
+  const arrayConnection = firstArray.concat(secondArray)
+  if (arrayConnection.length > maxLength) {
+    const message = arrayConnection.slice(0, maxLength);
+    return message;
+  } else {
+    return arrayConnection;
+  }
+}
 
-
-
-
-
+console .log (makeArray( [ "Mango" , "Poly" ] , [ "Ajax" , "Chelsea" ] , 3 )); // [ "Mango" , "Poly" , "Ajax" ] 
+console .log (makeArray( [ "Mango" , "Poly" , "Houston" ] , [ "Ajax" , "Chelsea" ] , 4 )); // [ "Mango" , "Poly" , "Houston" , " Ajax" ] 
+console .log (makeArray( [ "Mango" ] , [ " Ajax" , "Chelsea" , "Poly" , "Houston" ] )); // [ "Mango" , "Ajax" , "Chelsea" ] 
+console .log (makeArray( [ "Earth" , "Jupiter" ] , [ "Neptune" , "Uranus" ] , 2 )); // [ "Earth" , "Jupiter" ] 
+console .log (makeArray( [ "Earth" , "Jupiter" ] , [ "Neptune" , "Uranus" ] , 4 )); // [ "Earth " , "Jupiter" , "Neptune" , "Uranus" ] 
+console .log (makeArray( [ " Earth" , "Jupiter" ] , [ "Neptune" , "Uranus" , "Venus" ] , 0 )); // []
